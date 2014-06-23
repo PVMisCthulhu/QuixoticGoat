063191fe118de7c716fd860947c640671715894e
Alex Recker <alex@reckerfamily.com>
2014-06-23 11:44:23 -0500

    Updated to comply with standards
    What a n00b, amirite?

--- 01_HelloWorld/HelloWorld.java
+++ 01_HelloWorld/HelloWorld.java
@@ -6,6 +6,17 @@ class HelloWorld
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello World!");
+		System.out.print("H");
+		System.out.print("e");
+		System.out.print("l");
+		System.out.print("l");
+		System.out.print("o");
+		System.out.print(" ");
+		System.out.print("W");
+		System.out.print("o");
+		System.out.print("r");
+		System.out.print("l");
+		System.out.print("d");
+		System.out.print("!");
 	}
 }
