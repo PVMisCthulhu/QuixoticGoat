ab4098a460a7412318b974af491f064026171c93
Gene Roberts <groberts.pv2@gmail.com>
2014-06-25 11:57:45 -0500

    added comment

--- 00_MessingAround/Eclipse/SimpleJDBC/src/SimpleJDBC.java
+++ 00_MessingAround/Eclipse/SimpleJDBC/src/SimpleJDBC.java
@@ -7,6 +7,7 @@ public class SimpleJDBC
 {
 	// This application will select all the practice names from the DevDB01 database
 	// You need to have JDBC MSSQL 4.0 driver installed for this to work
+	// You will need to put sqlijdbc4.jar in your classpath
 	public static void main(String[] args) 
 	{
 		final String QUERY = "SELECT Practice FROM Practice ORDER BY Practice";
