8fa908689a38d61eae8e78bcd193adaef32914b2
Gene Roberts <groberts.pv2@gmail.com>
2014-06-26 09:47:19 -0500

    more android changes

--- 0A_Android/MyFirstApp/src/com/example/myfirstapp/MainActivity.java
+++ 0A_Android/MyFirstApp/src/com/example/myfirstapp/MainActivity.java
@@ -1,18 +1,21 @@
 package com.example.myfirstapp;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.EditText;
 
-public class MainActivity extends ActionBarActivity {
-
+public class MainActivity extends ActionBarActivity 
+{
+	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,13 +28,13 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);        
     }
 
     @Override
@@ -62,4 +65,12 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 
+    public void sendMessage(View view)
+    {
+    	Intent intent = new Intent(this, DisplayMessageActivity.class);
+    	EditText editText = (EditText)findViewById(R.id.edit_message);
+    	String message = editText.getText().toString();
+    	intent.putExtra(EXTRA_MESSAGE, message);
+    	startActivity(intent);
+    }
 }
